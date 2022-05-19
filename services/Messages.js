8148afa0dd890f011aa753171db10fa7ed3c74b3
Marcus Vinicius <gopokemonvinicius@gmail.com>
2022-05-19 17:44:01 -0300

    V1 chatbot

---
+++ services/Messages.js
@@ -0,0 +1,55 @@
+const textkeep = {}
+const defaultans = {}
+const defdef = ['Não reconheci o que você enviou.', 'Poderia responder minha pergunta anterior?']
+
+//talkat = 0
+textkeep.welcome = ['Olá! Sou um bot programado para auxiliar o processo de cadastro na pague',
+    'Vejo que seu contato não está salvo no meu banco de dados.', 
+    'Você gostaria de fazer seu cadastro']
+
+defaultans.welcome = ['Caso queria realizar um novo cadastrp, basta me enviar um "sim" ou ' +
+    '"Quero realizar uma nova matrícula" a qualquer momento.']
+
+
+//talkat = 1.0+
+textkeep.baseinfo = [
+    ['Tudo bem... Vou precisar de algumas informações suas agora.',                             //1.0
+    'Primeiramente eu vou requisitando cada uma em ordem e você vai me enviando.',
+    'Por favor, me informe o nome completo do aluno que fará a matrícula.'], 
+    ['Seu nome completo é ~nome~.', 'Você confirma?'],                                          //1.1
+    ['Certo, agora preciso do CPF do aluno.'], 
+    ['O CPF é ~cpf~. Você confirma?'],                                                          //1.2
+    ['Agora me informe a data de nascimento.', 'Só me enviar no formato dia/mes/ano'],          //1.3
+    ['Sua data de nascimento é ~datanas~. Você confirma?'],                                     //1.4
+    ['Tudo certo! O programa acaba aqui por enquanto.']]                                     
+
+defaultans.baseinfo = [[['Ok! Só enviar o seu nome completo.'],                                 //1.0       
+    ['Por favor, me envie o nome completo do aluno para que possamos prosseguir com o processo.'], 
+    ['Você poderia rever o nome enviado?', 'Apesar de eu ter pedido o seu nome completo' +
+    ', você me enviou apenas uma palavra.']], 
+    [['Certo, você poderia me envia o nome correto agora?']],                                   //1.1
+    [['Certo, Só me enviar o número do CPF.'], ['Eu preciso que você me envie o CPF ' + 
+        'correto.', 'Ele deve conter exatamente 11 dígitos.'], ['Por favor me envie o CPF correto' +
+        ' para que possamos prosseguir.']],                                                     //1.2
+    [['Tudo bem, pode enviar o CPF correto agora?']],                                           //1.3
+    [['Certo, só me enviar a data de nascimento.']],                                            //1.4
+    [['Tudo bem, pode me enviar a data de nascimento correta agora?']],                         //1.5
+    [['Não há mais respostas programadas atualmente.']],
+]
+
+//talkat = 2.0+
+textkeep.docs = [['Tudo certo então!', 'Agora vou precisar de '+
+    'alguns documentos, mas não precisa se preocupar caso não os tenha agora, você ' +
+    'poderá enviá-los quando quiser, basta me falar qual você ' +
+    'irá enviar a qualquer momento.', 'Esta é a lista de documentos que vou precisar:\n' + 
+    '   > Identidade;\n   > Certidão de nascimento;\n   > Histórico escolar.', 
+    'Você também pode me pedir para reenviar a lista a qualquer momento e eu também irei ' +
+    'informar quais você já enviou e quais faltam enviar. Você pode até mesmo me pedir para ' +
+    'te enviar um documento já entregue, para verificar se você me enviou o correto.', 
+    'Quando quiser me enviar algum documento basta falar "gostaria de enviar a identidade", ' +
+    'por exemplo, ou caso queira ver a lista de documentos ou um documento específico, só falar: ' +
+    '"Gostaria de ver a lista de documentos"/"Gostaria de ver a identidade enviada".']]
+
+
+//End
+export {textkeep, defaultans, defdef}
---
+++ services/Utils.js
@@ -0,0 +1,131 @@
+import {chat, ChatManager} from './ChatFlow.js'
+
+class Message{              //Guarda utilidades da mensagem recebida
+    constructor(str, type){
+        this.msgbody = str
+        this.wrds = this.cleanText(str)
+        this.wrdslen = this.wrds.length
+        this.type = type
+        this.positive = (['sim', 'ok', 'certo', 'beleza', 'concordo'].some((x) => this.wrds.includes(x)) || 
+             this.wrds.includes('tudo') && this.wrds.includes('bem')) &&
+             !this.wrds.includes('nao')
+        this.negative = (['nao', 'discordo', 'errado'].some((x) => this.wrds.includes(x))) &&
+            !this.wrds.includes('sim')
+        this.data = ''
+    }
+
+    checkTags(id){     //Testa se encontrou alguma das tags na mensagem
+        try{
+            if (/[~]/g.test(id)){
+                switch(id){
+                    case '~sim~':
+                        return this.positive
+                    case '~nao~':
+                        return this.negative
+                    case '~nome~':
+                        this.data = this.wrds.reduce((acc, i) =>{acc += 
+                            (!(['do', 'de', 'da'].includes(i)))?(i[0].toUpperCase()+i.slice(1)+' '):(i+' ')
+                            return acc},'').trim()
+                        return this.wrdslen > 1
+                    case '~cpf~':
+                        this.data = (this.msgbody.match(/\d/g)).reduce((acc, i)=>{acc+=i;return acc},'')
+                        return this.data.length == 11
+                    case '~def~':
+                        return true
+                    case '~num~':
+                        return /\d+/g.test(this.msgbody)
+                    case '~nop~':
+                        return false
+                }
+            } else {
+                return id.split('-').reduce((acc, i)=>{acc=(!this.wrds.includes(i)?false:acc);return acc},true)
+            }
+        }catch(err){
+            console.log(err)
+            return false
+        }
+    }
+
+    validateDate(){
+        let meses = ['janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho', 'julho', 
+            'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+        this.data = this.msgbody.match(/\d+/g)
+        if(this.data.length == 3){
+            this.data = this.data.reduce((acc, i, k)=>{acc += ((k!=2)?((i.length==1)?'0':''+`${i}/`):
+                ((i.length==2)?'19':''+`${i}`)); return acc}, '')
+        } else {
+            if(this.data.length == 1)
+                return false
+            else {
+                let mes = this.wrds.reduce((acc, i)=>{let x = meses.indexOf(i); acc=(x!=-1)?
+                    ((`${x+1}`.length!=2?'0':'')+`${x+1}`):acc; return acc}, 0)
+                if(parseInt(mes) == 0)
+                    return false
+                this.data = `${(this.data[0].length==1)?'0':''+`${this.data[0]}`}/${mes}/`+
+                    `${(this.data[1].length==2)?'19':''+`${this.data[1]}`}`
+            }
+        }
+        let info = this.data.split('/')
+        info[2] = (info[2].length == 2)?`19${info[2]}`:info[2]
+        info = info.map((i)=>parseInt(i))
+        let date = new Date()
+        let test = info[0]>0 && info[1]>0 && info[2]>0 && info[1] <= 12 &&
+            [1, 3, 5, 7, 8, 10, 12].includes(info[1])?(info[0]<=31):((info[1]==2)?
+            ((info[2]%4==0)?info[0]<=29:info[0]<=28):info[0]<=30)
+        test = test && (info[2] < (parseInt(date.getFullYear()) - 12))
+        return test
+    }
+
+    cleanText(str){
+        let txt = str.replace('ç', 'c')
+        return txt.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+            .replace(/[^a-z]/g, ' ').split(' ').reduce((acc, i)=>{(!i=='')?acc.push(i):null; return acc},[])
+    }
+}
+
+class DataBase{                 //Guarda todos os usuários
+    constructor(){
+        this.users = {}
+    }
+
+    /**
+     * @param {User} obj
+     */
+    set registerUser(obj){      //Registra um usuário
+        this.users[obj.id] = obj
+    }
+
+    resetUser(id){              //Reseta o usuário 
+        this.users[id] = null
+    }
+
+    chatManag(id){
+        return this.users[id].chat
+    }
+
+    newMessage(msg, id, first=false){
+        let stuff = this.chatManag(id).newMessage(msg, this.users[id].data, first)
+        switch(stuff.act){
+            case 'set-usertype-cpf':
+                this.users[id].type = 'cpf'
+                break
+            case 'validate-data':
+                this.users[id].data[stuff.type] = this.chatManag(id).data
+        }
+        return stuff.msg
+    }
+}
+
+class User{     //Guarda atributos sobre o usuário
+    constructor(num){
+        this.id = num
+        //this.number = num.split('@')[0].slice(-8)
+        this.type = 'new_user'
+        this.chat = new ChatManager()
+        this.data = {}
+        this.data['~nome~'] = null
+        this.data['~cpf~'] = null
+    }
+}
+
+export { Message, DataBase, User }
